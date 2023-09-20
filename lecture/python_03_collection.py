eee8a2a2c53aeab20393659cb34e2473b0ae9c1a
oknashin <144169609+oknashin@users.noreply.github.com>
2023-09-20 15:45:14 +0900

    python_03_collection.py

---
+++ lecture/python_03_collection.py
@@ -0,0 +1,18 @@
+# 컬렉션 타입
+#  - 변수 하나에 값을 여러개 저장
+#  - 실질적으로 변수에 컬렉션 1개가 저장
+#  - 리스트(List), 튜플(Tuple), 딕트 (Dictionary), 세트(set)
+
+# 1. 리스트(List)  ex)  기차!
+#  - 시퀸스 자료형 (연속된 값 저장)
+#  - 대가로 사용 []
+#  - 정렬 가능
+#  - mutable(생성된 후 변경 가능)
+#  - index 사용 (Slicing 가능)
+#  - paking과 unpaking 가능
+#  - 멤버함수: append(), extend(), insert(), remove(), pop(), index(), sorted(), 등등
+
+# 리스트 초기화(생성)
+list_a = [1, 2, 3]
+list_b = []
+list_c = ["chosun", 98, 3.14, [1, 2, 3]]
