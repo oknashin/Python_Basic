fe244ff3fc9ca7902d7958b7444ca173d81f91ef
oknashin <144169609+oknashin@users.noreply.github.com>
2023-10-06 12:36:50 +0900

    update practice_01_loop

--- lecture/pracice_01_loop.py
+++ lecture/pracice_01_loop.py
@@ -40,8 +40,60 @@ print(round(result, 2))  # 평균값
 
 # 문제 4) list b에서 최소값 찾기!
 b = [22, 1, 4, 7, 98]
-num_min = b[0]
-for num in b:
-    if num < num_min:
-        num_min = num
-print(num_min) # 1 출력
+num_min = b[0]  # 최소값 담을 공간
+for x in b:  # 5번 반복
+    if x < num_min:
+        num_min = x
+print(num_min)  # 1 출력
+
+# 문제 5) list c의 최소값, 최대값 찾기
+c = [2, 5, 7, 1, 8]
+num_min = c[0]
+num_max = c[0]
+for x in c:
+    if x < num_min:
+        num_min = x
+    if x > num_max:
+        num_max = x
+print(num_min)  # 1 출력
+print(num_max)  # 8 출력
+
+
+print("="*100)
+
+# 문제 6)사용자가 입력한 값 1, 2, 3 통과, 아닌 경우 다시 입력하도록
+
+# count = 0  # 틀린 횟수
+# while True:
+#    if count >= 4:
+#        print("프로그램을 사용할 수 없습니다.")
+#        break
+#    num = int(input("값: "))
+# if num == 1 or num != 2 or num != 3:
+#    if num in [1, 2, 3]:
+#        print(f"{num}을 입력하셨습니다.")
+#        break
+#   else:
+#        print("1, 2, 3의 값만 입력해주세요.")
+#        count += 1
+
+# 문제7) 1부터 100까지 총합을 출력하는 코드
+#  - for문으로 작성
+
+total = 0
+for num in range(1, 101):
+    total += num
+print(f"총합(for: {total}")
+
+#  - while문 작성
+
+num = 0
+total = 0
+
+while True:
+    num += 1
+
+    if num == 101:
+        break
+    total += num
+print(f"총합(while): {total}")
