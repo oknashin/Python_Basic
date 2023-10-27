9aa8d16eb47551bab7fac13cbab37569e7da240a
oknashin <144169609+oknashin@users.noreply.github.com>
2023-10-27 11:28:16 +0900

    parking_tower

--- lecture/python.main.py
+++ lecture/python.main.py
@@ -46,17 +46,25 @@ while True:
                     car_cnt += 1
                     break
         else:
-            print("MSG: 입고 불가, 다른 곳으로 가세요.")
+            print("MSG: 만차입니다. 다음에 이용해주세요.")
 
         pass
     elif select_num == 2:  # 출고
-        # 1. 차량번호
-        # 2. 주차타워 check 차량번호
-        #  2-1. X: 차 없음...
-        #  2-2. 출고(tower 값 = > ""), (car_cnt - 1)
-        pass
+        # 1.출고 할 차량번호 입력
+        car_num = input(">>출고: ")
+        # 2.차량번호 입고 된 차량인지 검색!
+        if car_num in tower:  # True -> 차량이 존재
+            for car in enumerate(tower):
+                if car == car_num:  # 출고
+                    tower[i] = ""  # 타워에서 차량제거
+                    car_cnt -= 1  # 현재 주차대수 동기화
+                    break
+        else:  # False -> 차량 x
+            print("MSG: 해당 번호로 입고 된 차량이 없습니다.")
     elif select_num == 3:  # 조회
-        pass
+        print("== 주차 타워 현황 == ")
+        for i in range(len(tower)-1, -1, -1):
+            print(f"> {i+1}층 {tower[i]}")
     elif select_num == 4:  # 종료
         print("MSG: 프로그램을 종료합니다.")
         exit()
