3a513a7cca9109cea0bd9020f8ff390c39eedb50
oknashin <144169609+oknashin@users.noreply.github.com>
2023-11-08 15:04:03 +0900

    dawm_news_list.py

--- lecture/daum_news_list.py
+++ lecture/daum_news_list.py
@@ -5,21 +5,31 @@
 
 import requests  # 전체 소스코드
 from bs4 import BeautifulSoup  # 원하는 정보 SELECT
-from service.service_news import get_news
+from service_news import get_news
 
 count = 0  # 전체 기사수
-url = "https://news.daum.net/breakingnews/digital"
-result = requests.get(url)
+page = 1  # 시작 페이지 1로 고정
 
-if result.status_code == 200:
-    print(result, "접속 성공 -> 데이터를 수집합니다.")
+while True:
+    url = f"https://news.daum.net/breakingnews/digital?page={page}"
+    result = requests.get(url)
 
-    doc = BeautifulSoup(result.text, "html.parser")
-    url_list = doc.select("ul.list_news2 a.link_txt")
-    for url in url_list:
-        count += 1
-        print(f"{count+1}", "="*100)
-        # get_news(): 기사 제목
-        get_news(url["href"])
+    if result.status_code == 200:
+        print(result, "접속 성공 -> 데이터를 수집합니다.")
+
+        doc = BeautifulSoup(result.text, "html.parser")
+        url_list = doc.select("ul.list_news2 a.link_txt")
+        # print(f"{page} 페이지의 기사 갯수: {len(url_list)}")
+
+        if len(url_list) == 0:  # 마지막 페이지
+            break
+
+        for url in url_list:
+           count += 1
+           print(f"{count+1}", "="*100)
+           # get_news(): 기사 제목, 본문, 날짜 수집 함수
+           get_news(url["href"])
     else:
-        print("URL 경로가 잘못됐습니다. 확인부탁드립니다.")
+            print("URL 경로가 잘못됐습니다. 확인부탁드립니다.")
+
+    page += 1
