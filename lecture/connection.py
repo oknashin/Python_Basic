a8122abd4c8860adac404ed421ea513029621d02
oknashin <144169609+oknashin@users.noreply.github.com>
2023-11-22 15:34:46 +0900

    mariaDB connection.py

--- lecture/connection.py
+++ lecture/connection.py
@@ -1,64 +1,46 @@
-# 데이터베이스(DB): 데이터를 효율적으로 저장하는 시스템
-# FileSystem: 폴더를 사용한 저장
+# DB: MariaDB
+#  **root:
+#    - RDB계열에 모두 존재하는 계정
+#    - 최상위 계정(신급 권한을 가진 계정)
 
-# 데이터베이스관리시스템(DBMS): DB이론을 실제로 구현!
+# 예) 문서확인(제 보안등급으로 확인이 불가!!)
+# DB에는 개인정보뿐만 아니라 중요한 데이터가 저장
+# 아무나 접근하게 하지 않음
+# 계정별로 권한을 주고 접근할 수 있는 DB 및 테이블과 기능을 제한함!
 
-# ** DBMS 종류
-#  1) 관계형 데이터베이스(RDB): 전통적인(스키마: 명세서)
-#   - ORACLE
-#   - MySQL
-#   - MariaDB
+# 개발1팀 조직
+# 팀장: tl_01(tbl_member:WR, tbl_board:WR)
+#  ㄴ 대리: tl_02(tbl_member:WR)
+#  ㄴ 사원: tl_03(tbl_board:WR)
+#  ㄴ 신입사원2: tl_04(tbl_member:R, tbl_board:R)
 
-#  2) NoSQL: NEW
-#   - MongoDB
-#   - Redis
+# Python -pymysql ----------------- MariaDB
 
-# ** DB 프로세스
-#  - Do: ID와 PW 저장
-#  Pycharm(Python)  ------------------ DB(MongoDB)
-#   1) Pycharm과 DB가 Connection 맺기
-#      - IP: 컴퓨터를 찾아갈 수 있는 주소!
-#      - PORT: 컴퓨터 내에서 특정 프로그램의 위치
-#      - ID and PW
-#   2) SQL을 사용해서 DB에 CRUD작업 진행
-#      - SQL(구조질의어): DB와 소통할 수 있는 언어(반드시)
-#      - RDB(SQL)을 사용, NoSQL(SQL X)
-#      Create(삽입)  -> INSERT
-#      Read(조회)    -> SELECT
-#      Update(수정)  -> UPDATE
-#      Delete(삭제)  -> DELETE
+# ** COMMIT
+#  - 원본문서 -> "HI" 추가 -> 저장 -> 원본문서(업데이트)
+#  - 원본문서 -> "HI" 추가 -> 저장X -> 원본문서(기존과 동일)
 
-# ** MongoDB 설정 방법
-#  1.직접 설치(Local)
-#  2.MongoDB에서 제공하는 웹 클라우드 사용(설치 x)
-#    - MongoDB 회원가입 필수!
-#    - IP와 PORT => URL 제공!
+# TABLE -> INSERT, UPDATE, DELETE -> COMMIT(X) -> 변경 X
 
-#  ** MongoDB 구조
-#  설치: MongoDB(DBMS): 전체 시스템
-#         ㄴ DB(카카오톡): 폴더
-#            ㄴ collection(회원): 표
-#            ㄴ collection(톡):
-#            ㄴ collection(선물):
-#            ㄴ collection(친구):
-#         ㄴ DB(카카오뱅크)
-#            ㄴ collection(회원):
-#            ㄴ collection(계좌):
-#            ㄴ collection(대출):
-#         ㄴ DB(카카오페이)
-#         ㄴ ...
-# ※ 데이터를 CRUD -> collection
+# 우리 DB 구조
+# MariaDB(DBMS)
+#  ㄴ daum(Database)
+#      ㄴ tbl_review(Table)
 
+import pymysql
 
-from pymongo import MongoClient
-
-# Mongo DB Connection
-def conn_mongodb():
-    # ID, PORT, ID, PW
-    DB_ID = ""  # 상수(전체 대문자로 변수명을 사용)
-    DB_PW = ""  # ex) 은행권 => 금리(상수)
-    client = MongoClient(f"mongodb+srv://{DB_ID}:{DB_PW}@atlascluster.1jknaiy.mongodb.net/")
-    # DB설정 -> collection 설정
-    db = client["daum"]
-    collection = db.get_collection("news")
-    return collection
+def connection():
+    try:
+        conn = pymysql.connect(
+            host="127.0.0.1",
+            port=3306,
+            user="root",
+            password="1234",
+            db = "daum",
+            charset="utf8",
+            autocommit=True,
+            cursorclass=pymysql.cursors.DictCursor
+        )
+        return conn
+    except pymysql.Error as e:
+        print(f"MARIADB 연결 실패 {e}")
