ee677b97efdeae0988966ad4e7a5d71ee21f45f1
oknashin <144169609+oknashin@users.noreply.github.com>
2023-11-24 12:25:19 +0900

    mariaDB.connection.py

--- lecture/connection.py
+++ lecture/connection.py
@@ -35,8 +35,8 @@ def connection():
             host="127.0.0.1",
             port=3306,
             user="root",
-            password="1234",
-            db = "daum",
+            password="12341234",
+            db="daum",
             charset="utf8",
             autocommit=True,
             cursorclass=pymysql.cursors.DictCursor
